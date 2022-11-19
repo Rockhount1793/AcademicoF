f245c5f50a10eefc8a9285572bc31dda06fbdf11
leo <leocj@live.com.co>
2022-11-19 17:55:25 -0500

    rutinario

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ import env from '@/config/env.js'
 
 const Config = {
 
-    'version':'1.0.0.0 17-11-2022 19:35',
+    'version':'1.0.0.0 19-11-2022 17:50',
 
     // development o production
     'status': 'development',
