ccdad4ee8dcb0a96b5791e90eca439e83b7c0f63
Yeison Caicedo <yeison@tryjeeves.com>
2023-05-28 19:30:21 -0500

    added production logic

--- src/config/env.js
+++ src/config/env.js
@@ -2,8 +2,8 @@
 const Development = {
 
     'app':{
-        'urlsf': "http://localhost:8080",
-        'urlsb': "http://localhost:8000",
+        'urlsf': import.meta.env.VITE_URLSF,
+        'urlsb': import.meta.env.VITE_URLSB,
         'token_header_key' : "_token",
     }
 
